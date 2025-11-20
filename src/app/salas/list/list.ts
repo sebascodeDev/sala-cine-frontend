3c60182d3b0260bc93b877a7e5c259ec258dc994
Sebastian Valarezo <sebastianvalarezomariscal2004@gmail.com>
2025-11-20 05:06:36 -0500

    feat(salas): implementa CRUD de salas con listado y edición

---
+++ src/app/salas/list/list.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+interface Sala {
+  id: number;
+  nombre: string;
+  capacidad: number;
+}
+
+@Component({
+  selector: 'app-list',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './list.html',
+  styleUrl: './list.css',
+})
+export class List {
+  salas: Sala[] = [
+    { id: 1, nombre: 'Sala A', capacidad: 100 },
+    { id: 2, nombre: 'Sala B', capacidad: 80 }
+  ];
+}
