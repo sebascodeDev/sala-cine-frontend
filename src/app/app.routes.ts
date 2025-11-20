3b47f03bb205e3a07ad7c86bc8c4e166d90290dc
Sebastian Valarezo <sebastianvalarezomariscal2004@gmail.com>
2025-11-20 05:06:55 -0500

    feat(routing): configura rutas y menú de navegación principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Login as LoginComponent } from './auth/login/login';
+import { Dashboard as DashboardComponent } from './dashboard/dashboard/dashboard';
+import { List as PeliculasListComponent } from './peliculas/list/list';
+import { Edit as PeliculaEditComponent } from './peliculas/edit/edit';
+import { List as SalasListComponent } from './salas/list/list';
+import { Edit as SalaEditComponent } from './salas/edit/edit';
+import { Asignar as AsignarComponent } from './asignaciones/asignar/asignar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'peliculas', component: PeliculasListComponent },
+  { path: 'peliculas/nuevo', component: PeliculaEditComponent },
+  { path: 'peliculas/:id', component: PeliculaEditComponent },
+  { path: 'salas', component: SalasListComponent },
+  { path: 'salas/nuevo', component: SalaEditComponent },
+  { path: 'salas/:id', component: SalaEditComponent },
+  { path: 'asignaciones', component: AsignarComponent }
+];
